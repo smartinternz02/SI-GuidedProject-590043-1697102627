76236d3ca7b34405b4810306448baf4c5891409d
Gone Srikanth <141805867+Gonesrikanth0811@users.noreply.github.com>
2024-05-27 08:42:41 +0530

    Delete The Sleep Oracle Anticipating Health and Lifestyle through Data/t.py

--- The Sleep Oracle Anticipating Health and Lifestyle through Data/t.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template, request
-import pickle
-import numpy as np
-import warnings
-warnings.filterwarnings('ignore')
-
-app = Flask(__name__)
-model = pickle.load(open('xgbc.pkl', 'rb'))
-print(model.predict([[1,20,3,0,8,23,4,0,80,32000,120,80]]))
